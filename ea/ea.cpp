8e5683de202ccb51f69e908e6c86723762c07cfe
NikitaVisheratin <visheratinn@gmail.com>
2021-10-07 23:52:14 +0300

    ea commit

---
+++ ea/ea.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main()
+{
+    int i, N;
+    double x_1, y_1, x_2, y_2;
+    std::cin >> N;
+    for (i = 1; i <= N; i++)
+    {
+        std::cin >> x_1 >> y_1 >> x_2 >> y_2;
+        std::cout << (x_1 + x_2) / 2 << " " << (y_1 + y_2) / 2;
+    }
+    return 0;
+}
